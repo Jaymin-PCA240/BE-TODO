56968615745eb719add0489114e838247e863c25
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-08-20 12:24:44 +0530

    fixed code

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,9 @@ set -e
 
 APP_DIR="/home/ec2-user/app"
 
+echo "Running Prisma migrations..."
+npx prisma migrate deploy
+
 echo "=== Installing production dependencies ==="
 cd $APP_DIR
 npm ci --only=production
