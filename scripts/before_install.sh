669bc44a7848abe0094fdc4e359f426fb3b8b318
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-08-20 15:58:37 +0530

    fixed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
+echo "=== Cleaning old application files but keeping .env ==="
+cd /home/ec2-user/app
 
-echo "=== Cleaning old application files ==="
-rm -rf /home/ec2-user/app/*
+# Enable extended pattern matching
+shopt -s extglob
+
+# Remove everything except .env
+sudo rm -rf !( .env )
+
+echo "=== Cleanup complete ==="
+
